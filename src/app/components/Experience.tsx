3fa2b7beef6626eede2c755d75d264c3270158f5
mawaisahmad <118178925+mawaisahmad@users.noreply.github.com>
2023-06-25 10:45:16 +0500

    Update Experience.tsx

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -30,8 +30,8 @@ import Image from "next/image";
 return (
   <main className='flex w-full flex-col items-center justify-center'> 
   <div className="grid w-11/12 grid-cols-8 gap-16">
-    <div className="col-span-6 lg:col-span-5 flex flex-col items-start justify-around gap-2  md:order-2">
-<li ref={ref} className='mb-16 lg:p-8 font-medium text-left dark:text-light rounded-2xl border border-solid dark:border-dark  bg-dark/10 dark:bg-light/20 p-6 shadow-md shadow-dark/50'> 
+    <div className="col-span-6 lg:col-span-6 flex flex-col items-start justify-around gap-2  md:order-2">
+<li ref={ref} className='mb-16 lg:p-2 font-medium text-left dark:text-light rounded-2xl border border-solid dark:border-dark  bg-dark/10 dark:bg-light/20 p-6 shadow-md shadow-dark/50'> 
 <LiIcon reference={ref} />
 <div>
 <h3 className='capitalize font-bold text-2x1'>
@@ -61,17 +61,18 @@ const Experience = () => {
   )
 return (
 
-     <div className='mt-8 mb-4'>
+     <div className=''>
     
       <div>
-        <AnimatedText text="Experience" className="!text-6xl lg:!text-4xl text-dark font-bold text-center" />
+       
         <div className='w-full relative'>
                     <motion.div
             style={{ scaleY: scrollYProgress }}
             className="absolute left top-14 -bottom-2 ml-16 lg:ml-8 w-[4px] h-[88%] bg-dark dark:bg-primary origin-top"
           />
           <ul className="ml-16">
-            <Details 
+             <AnimatedText text="Experience" className="!text-6xl lg:!text-4xl text-dark font-bold text-center mb-8 lg:text-start" />
+                        <Details 
               position="Web Developer"
               company="Fiverr"
               companyLink="www.fiverr.com"
@@ -123,7 +124,7 @@ return (
                Through my leadership, I promoted excellence, 
                encouraged innovation, and facilitated the achievement of our institute's goals."
             />
-            <AnimatedText text="Education" className="!text-6xl lg:!text-4xl text-dark font-bold text-center" />
+            <AnimatedText text="Education" className="!text-6xl lg:!text-4xl text-dark font-bold text-center mb-8 lg:text-start" />
             <Details 
               position="MBA-Finance (Gold Medalist)"
               company="Iqra University"
